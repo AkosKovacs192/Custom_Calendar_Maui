b6f6dd2c2c6a2cb68977f0c35cc06cb76df48718
AkosKovacs192 <102877635+AkosKovacs192@users.noreply.github.com>
2023-07-03 21:59:55 +0100

    Day interface setup

---
+++ Library/Custom_Calendar_Library/Day.cs
@@ -0,0 +1,26 @@
+﻿using Custom_Calendar_Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Calendar_Library
+{
+    public class Day : IDay
+    {
+
+        public void Select() { }
+        public void DeSelect() { }
+        public bool IsSelected() { return false; }
+        public DateTime GetDateTime() { return new DateTime(); }
+        public DateOnly GetDateOnly() { return new DateOnly(); }
+       
+        public string GetDayOfTheWeek() { return string.Empty; }
+  
+        public int GetIndexOfTheWeek() { return 0; }
+        public bool IsActive() { return false;  }
+        public void Activate() { }
+        public void Deactivate() { }
+    }
+}
---
+++ Library/Custom_Calendar_Library/Interfaces/IDay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Calendar_Library.Interfaces
+{
+    public interface IDay
+    {
+
+        public void Select();
+        public void DeSelect();
+        public bool IsSelected();
+        DateTime GetDateTime();
+        DateOnly GetDateOnly();
+        /// <summary>
+        /// Monday
+        /// Tuesday
+        /// ...
+        /// </summary>
+        /// <returns></returns>
+        string GetDayOfTheWeek();
+        /// <summary>
+        /// 0 == Monday
+        /// 1 == Tuesday
+        /// ...
+        /// </summary>
+        /// <returns></returns>
+        int GetIndexOfTheWeek();        
+        bool IsActive();
+        void Activate();
+        void Deactivate();
+
+    }
+}
